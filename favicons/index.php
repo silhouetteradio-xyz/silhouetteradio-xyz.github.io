51b0892a97a233e0cc167aa5aed6d71060bf845a
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-01-30 17:35:09 +1300

    Add files via upload

---
+++ favicons/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact | Silhouette</title>
+    <link href="https://beatsturning.com/style/404.css" rel="stylesheet" type="text/css" />
+	<?php include '../head.php'; ?>
+	
+  </head>
+  <body>
+    <div></div>
+	<h1 id=title>Yeah we have no idea what you're looking for</h1>
+	<h2 id=error>404 | page not found</h2>
+	<div id=home-container>
+	  <a id=home href="https://beatsturning.com">home</a>
+	</div>
+	<h2 id=other>Other Pages:</h2>
+	<div id=links>
+	  <a href="https://beatsturning.com/legal">Terms of Use</a>
+	  <a href="https://beatsturning.com/legal/#copyright">Copyright</a>
+	  <a href="https://beatsturning.com/privacy">Privacy Policy</a>
+	  <a href="https://beatsturning.com/jobs">Jobs</a>
+	  <a href="https://beatsturning.com/developer">Developer</a>
+	  <a href="https://beatsturning.com/contact">Contact</a>
+	  <a href="https://beatsturning.com/jobs/#feature">Get Featured</a>
+	  <a href="https://beatsturning.com/contact">Advertising Enquiries</a>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	</div>
+	<div style="display:none">
+  </body>
+</html>
